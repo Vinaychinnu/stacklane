1407eeae868c1498b2d61b9d0988c638e97a91ae
Vinaychinnu <kombathulavinaykumar@gmail.com>
2026-05-25 11:42:55 +0530

    feat: add initial health endpoint

--- services/service-registry/main.go
+++ services/service-registry/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	
+	"log"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request){
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
+}
 
 func main() {
-	fmt.Println("Service Registry started")
+	http.HandleFunc("/health", healthHandler)
+
+	log.Println("Service-registry started on :8082")
+
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
